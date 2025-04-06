49cabd8f3990023f92199908f3add5543dfa6941
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-04-06 11:41:37 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,42 @@
 are_isomorphic(graph1, graph2) {
-    return undefined;
+    if (graph1.length != graph2.length) {
+        return false;
+    }
+
+    var perm = findPerm(graph1.length);
+
+    for (var x of perm) {
+        if(check(x, graph1, graph2)) {
+            return true;
+        }
+            
+    return false;
 }
+
+findPerm (graph1) {
+    var recurPerm = [];
+    if (graph1.length <= 1) {
+        return [graph1];
+    }
+    
+
+    for (var i = 0; i < graph1.length; i++) {
+        var partial = findPerm(graph1.slice(0, i).concat(graph1.slice(i + 1)));
+        for (var x of partial) {
+            recurPerm.push([graph1[i]].concat(x));
+        }
+    }
+    return recurPerm;
+}
+
+check(x, graph1, graph2) {
+    for (var o = 0; o < x.length; o++) {
+        for (var i = 0; i < x.length; i++) {
+            if (graph1[o][i] != graph2[x[o]][x[i]]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
